195dde15cf0e706fb05c1ac4bebf86a2977ee95a
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-03-25 10:20:55 -0300

    Update Tuplas.py

--- Tuplas.py
+++ Tuplas.py
@@ -44,6 +44,7 @@ print("Estado:", cliente_1[2])
 print("RG:", cliente_1[3])
 print("Ativo:", cliente_1[4])
 '''
+'''
 # Funções para verificação
 nums2 = (5,3,5,65,223,67,-54,223)
 print(len(nums2))           # len - Quantidade de elementos
@@ -51,3 +52,14 @@ print(nums2.count(5))       # Quantidade de um elementos específico
 print(nums2.index(223))     # Primeiro índice de um elemento
 print(sum(nums2))           # Calcula a soma dos elementos numéricos
 print(sum(nums2)/len(nums2))# Calcular a média dos números
+'''
+# Lista de Tuplas (2D Matriz)
+cliente_1 = ("Marcos", 54, "SP", "4623897",False)
+cliente_2 = ("Yasmin",27,"RJ","1248965", True)
+
+cadastro = []               # Declaração de lista vazia
+cadastro.append(cliente_1)  # Adicionar tupla de cliente_1 na lista
+cadastro.append(cliente_2)  # Adicionar tupla de cliente_2 na lista
+print(cadastro)
+print(cadastro[0])          # Printa a linha toda
+print(cadastro[1][0])       # Printa elemento da linha e coluna
