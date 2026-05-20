88a36cb20ffb59e00c6f527353543a18f2e1ae28
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-05-20 10:31:08 -0300

    Create Carro.py

---
+++ POO/Veiculos/veiculos/terrestre/Carro.py
@@ -0,0 +1,14 @@
+from terrestre.Terrestre import Terrestre
+
+class Carro(Terrestre):
+    def __init__(self, nome, numeroRodas, tipoCombustivel):
+        super().__init__(nome, numeroRodas)
+        self._tipoCombustivel = tipoCombustivel
+
+    def mover(self):
+        print(f"O carro {self._nome} está se movendo com a velocidade de {self._velocidade} km/h.")
+
+    def mostrarInfo(self):
+        super().mostrarInfo()
+        print("Número de rodas:",self._numeroRodas)
+        print("Tipo de combustível:",self._tipoCombustivel)
