d836fa99c382285c58228cb3ab680d64f817653a
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-05-27 08:49:49 -0300

    Update main.py

--- POO/Veiculos/main.py
+++ POO/Veiculos/main.py
@@ -1,5 +1,17 @@
 from veiculos.Veiculo import Veiculo
+from veiculos.terrestre.Terrestre import Terrestre
+from veiculos.terrestre.Carro import Carro
 
 veiculo_1 = Veiculo("Patinete")
 veiculo_1.acelerar(10)
 veiculo_1.mostrarInfo()
+
+terrestre = Terrestre("Trator",4)
+terrestre.acelerar(5)
+print(terrestre._nome)
+terrestre.mostrarInfo()
+
+carro = Carro("Opala",4,"Gasolina")
+carro.acelerar(80)
+carro.desacelerar(20)
+carro.mostrarInfo()
