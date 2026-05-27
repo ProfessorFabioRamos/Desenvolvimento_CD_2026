94a5a9362ccea404d3e44d3f5d18f7a78f31dc50
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-05-27 09:20:20 -0300

    Create JatoGuerra.py

---
+++ POO/Veiculos/veiculos/aereo/JatoGuerra.py
@@ -0,0 +1,24 @@
+from veiculos.aereo.Aereo import Aereo
+
+class JatoGuerra(Aereo):
+    def __init__(self, nome, quantidadeMisseis):
+        super().__init__(nome)
+        self.quantidadeMisseis = quantidadeMisseis
+
+    def atirarMissel(self):
+        if self.quantidadeMisseis <=0:
+            print("Não há mais misseis no Jato")
+        else:
+            self.quantidadeMisseis -= 1
+            print("Missil atirado. Misseis restantes:",self.quantidadeMisseis)
+    
+    def ganharAltitude(self, incremento):
+        if self._velocidade >= 1000:
+            super().ganharAltitude(incremento)
+        else:
+            print("E necessario mais velocidade para ganhar altitude")
+
+    def perderAltitude(self, decremento):
+        super().perderAltitude(decremento)   
+        if self.altitude < 0:
+            self.altitude = 0
