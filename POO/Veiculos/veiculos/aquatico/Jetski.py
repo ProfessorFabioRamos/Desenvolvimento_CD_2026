a3b051f15da6cce555bd2302d7cbd5679bdafd0c
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-05-27 09:37:40 -0300

    Create Jetski.py

---
+++ POO/Veiculos/veiculos/aquatico/Jetski.py
@@ -0,0 +1,20 @@
+from veiculos.aquatico.Aquatico import Aquatico
+
+class Jetski(Aquatico):
+    def __init__(self, nome, possuiHelice, tripulantes):
+        super().__init__(nome, possuiHelice)
+        self.tripulantes = tripulantes
+
+    def embarcar(self, quantidade):
+        if quantidade > 2 or self.tripulantes == 2:
+            print("Não é possível embarcar esta quantidade")
+        else:
+            self.tripulantes += quantidade
+    
+    def desembarcar(self, quantidade):
+        self.tripulantes -= quantidade
+        if self.tripulantes < 0:
+            self.tripulantes = 0
+        
+
+    
